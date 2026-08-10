26a45dcc5a6bbcd427c5f4e3e40d133e61aa25ef
Gabriela <gabiperigocararo@gmail.com>
2026-08-10 21:19:35 +0000

    servidor criado com susseso!

---
+++ servidor-http-nativo/server.js
@@ -0,0 +1,14 @@
+const http = require('node:http');
+const PORTA = 3000;
+const server = http.createServer((req, res) => {
+console.log(`Requisição recebida! ${req.method} ${req.url}`);
+
+res.statusCode = 200;
+res.setHeader('Content-Type' , 'text/plain; charset=utf-8');
+
+res.end("Servidor nativo funcionado!")
+    });
+
+    server.listen(PORTA, ()=>{
+        console.log(`Servidor funcionndo na porta ${PORTA}`);
+    })
